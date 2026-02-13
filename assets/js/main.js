6643bb22bab0e6bf7e634d3c668944be7fd12964
frasheritm <frasheritm@gmail.com>
2026-02-14 00:48:05 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
-
+(() => {
+  const btn = document.querySelector('[data-menu-btn]');
+  const links = document.querySelector('[data-nav-links]');
+  if (btn && links) {
+    btn.addEventListener('click', () => links.classList.toggle('open'));
+  }
+})();
